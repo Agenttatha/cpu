99a3dd3fa6ff06baca3cc459a94cc25922e5e78f
Agenttatha <142716423+Agenttatha@users.noreply.github.com>
2023-12-04 23:57:25 +0530

    Add files via upload

---
+++ paging.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 50
+int main()
+{
+int page[MAX],i,n,f,ps,off,pno;
+int choice=0;
+printf("\nEnter the no of pages in memory: ");
+scanf("%d",&n);
+printf("Enter page size: ");
+scanf("%d",&ps);
+printf("Enter no of frames: ");
+scanf("%d",&f);
+for(i=0;i<n;i++)
+        page[i]=-1;
+printf("\nEnter the page table\n");
+printf("(Enter frame no as -1 if that page is not present in any frame)\n\n");
+printf("\npageno\tframeno\n-------\t-------");
+for(i=0;i<n;i++)
+{
+        printf("\n%d\t",i);
+        scanf("%d",&page[i]);
+        if(page[i] >= f){
+                printf("Frame no. is greater than no. of frames\nexiting...");
+                exit(0);
+        }
+}
+do
+{
+        printf("\nEnter the logical address(i.e,page no & offset):");
+        scanf("%d%d",&pno,&off);
+        if(page[pno]==-1)
+                printf("\nThe required page is not available in any of frames");
+        else
+                printf("\nPhysical address(i.e,frame no & offset):%d,%d",page[pno],off);
+        printf("\nDo you want to continue(1/0)?:");
+        scanf("%d",&choice);
+}while(choice==1);
+return 1;
+}
